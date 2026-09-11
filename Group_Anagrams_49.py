3d991e7db884e75b1d63abfb4b14256bec5cd65f
aislam5 <abdullahislam900@gmail.com>
2026-09-11 15:30:20 -0400

    Added Valid Solution to Problem 49 - Group Anagrams and also updated file names to ensure executablitiy

---
+++ Group_Anagrams_49.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        #make all hashamps and then compare 
+        my_dict = defaultdict(list)
+        for word in strs:
+            count = [0]*26 #make a array every word 
+            for char in word:
+                count[ord(char)-ord('a')] += 1 #the stuff inside the square brackets gives us the index for the letter using ASCII values
+            my_dict[tuple(count)].append(word) #we make count a tuple since a lsit is not a hashable object
+        return list(my_dict.values())
+
+if __name__ == "__main__" :
+    solution = Solution()
+    test = ["eat","tea","tan","ate","nat","bat"]
+    hi = solution.groupAnagrams(test)
+    print(hi)
+
+
